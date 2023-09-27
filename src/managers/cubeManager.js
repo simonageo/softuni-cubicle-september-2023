f24a21d188638a6fd85ebc11fc7d4d0b9c63a051
simonageo <sag954@gmail.com>
2023-09-27 21:59:17 +0300

    Show cubes on home page dynamically

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,10 +1,10 @@
-const uniqid=require('uniqid');
-const cubes=[];
+const uniqid = require('uniqid');
+const cubes = [];
 
-exports.getAll=()=>cubes.slice();//returns a shallow copy of the array cubes
+exports.getAll = () => cubes.slice();//returns a shallow copy of the array cubes
 
-exports.create=(cubeData)=>{
-    const newCube={
+exports.create = (cubeData) => {
+    const newCube = {
         id: uniqid(),
         ...cubeData
     };//adds an id to the cube
